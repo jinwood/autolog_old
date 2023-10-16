71b09e65c5f0e00e4fc5d0553745cb1ff13462b9
jinwood <jinw@protonmail.com>
2023-10-16 11:46:24 +0100

    Add media form

---
+++ src/pages/vehicles/media/[id]/add/index.tsx
@@ -0,0 +1,34 @@
+import { useRouter } from "next/router";
+import Container from "~/components/container";
+import { useImageUpload } from "~/hooks/useUploadImage";
+
+export default function AddVehicleMediaPage() {
+  const id = useRouter().query.id;
+
+  const {
+    selectedFile,
+    handleFileChange,
+    uploadImage,
+    uploading,
+    uploadError,
+  } = useImageUpload(String(id));
+
+  return (
+    <Container>
+      <h2 className="mb-4 text-xl font-semibold">Image Upload</h2>
+      <input
+        type="file"
+        accept="image/*"
+        onChange={(e) => handleFileChange(e.target.files?.[0] ?? null)}
+      />
+      <button
+        className="mt-4 rounded-md bg-blue-500 p-2 text-white"
+        onClick={void uploadImage}
+        disabled={uploading || !selectedFile}
+      >
+        {uploading ? "Uploading..." : "Upload Image"}
+      </button>
+      {uploadError && <p className="mt-2 text-red-500">{uploadError}</p>}
+    </Container>
+  );
+}
